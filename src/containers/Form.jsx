3d1a5a0cc77553d14f95b887395a116a79c5950f
mrglox <contact@morgan-leroux.com>
2022-10-06 11:18:20 +0200

    add comments

--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -4,14 +4,25 @@ import List from "components/List";
 import data from "data.json";
 
 export default function Form() {
+  // state to handle value from input
   const [value, setValue] = useState("");
+
+  // state to handle filtered apparts
   const [filteredApparts, setFilteredApparts] = useState(data);
 
+  // function triggered at input value change
   const handleChange = (ev) => {
+    // sync state value with input
     setValue(ev.target.value);
+
+    // sync filtered apparts with input value
     setFilteredApparts(
-      data.filter((appart) =>
-        appart.title.toLowerCase().includes(ev.target.value.toLowerCase())
+      data.filter(
+        (
+          appart // one appart by one
+        ) =>
+          // filter condition
+          appart.title.toLowerCase().includes(ev.target.value.toLowerCase())
       )
     );
   };
@@ -19,9 +30,11 @@ export default function Form() {
   return (
     <>
       <div>
+        {/* input for filter */}
         <input type="text" onChange={handleChange} />
         <p>{value}</p>
       </div>
+      {/* display list */}
       <List apparts={filteredApparts}></List>
     </>
   );
